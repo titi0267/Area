032b1a2e5cdbf0849e369eef2cab168026e07774
coniel <timothe@coniel.com>
2022-10-04 19:01:55 +0200

    Feat: Area list

---
+++ back/src/routes/area.route.ts
@@ -0,0 +1,22 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { FastifyPluginOptions } from "fastify/types/plugin";
+
+import { FastifyPluginDoneFunction } from "../types/global.types";
+import { AreaService } from "../services";
+import httpStatus from "http-status";
+
+export default (
+  instance: FastifyInstance,
+  _opts: FastifyPluginOptions,
+  done: FastifyPluginDoneFunction,
+): void => {
+  instance.get("/", async (req: FastifyRequest, res: FastifyReply) => {
+    const areas = await AreaService.getAllAreas();
+
+    res.status(httpStatus.OK).send(areas);
+  });
+
+  done();
+};
+
+export const autoPrefix = "/areas";

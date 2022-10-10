1ca9d52ac64cca31f577a18f0b9e0153c2dc67fc
MaximeHff <maxime.hoffbeck@epitech.eu>
2022-10-10 23:27:53 +0200

    fix: router.ts

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -11,7 +11,6 @@ import store from "../store";
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-<<<<<<< HEAD
     mode: 'history',
     base: '/',
     routes: [
@@ -68,62 +67,6 @@ router.beforeEach(async (to, from, next) => {
         }
     }
     next();
-=======
-  mode: "history",
-  base: "/",
-  routes: [
-    {
-      path: "/register",
-      name: "register",
-      component: Register,
-      meta: { requiresAuth: false },
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: Login,
-      meta: { requiresAuth: false },
-    },
-    {
-      path: "/user-pannel",
-      name: "userPannel",
-      component: UserPannel,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: "/create-area",
-      name: "createArea",
-      component: CreateArea,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: "/",
-      redirect: "/user-pannel",
-      meta: { requiresAuth: false },
-    },
-    {
-      path: "/:pathMatch(.*)*",
-      name: "notFound",
-      component: NotFound,
-      meta: { requiresAuth: false },
-    },
-  ],
->>>>>>> 458f15798e26c584d5de5fd1c510de65a20f7b82
-});
-
-router.beforeEach(async (to, from, next) => {
-  let usrToken = localStorage.getItem("usr-token");
-  if (usrToken != null) store.commit("updateToken", usrToken);
-  if (to.meta && to.meta.requiresAuth == true) {
-    if (usrToken === null) router.push("login");
-    try {
-      await axios.get("/users/areas");
-      next();
-    } catch (err) {
-      router.push("login");
-    }
-  }
-  next();
 });
 
 export default router;

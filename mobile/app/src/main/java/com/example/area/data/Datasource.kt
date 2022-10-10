2a9ad29b34def7a24598d059cf6720115e5818c4
Frantz <alexandre.frantz@epitech.eu>
2022-10-10 09:29:41 +0200

    Refacto: remove test files

--- mobile/app/src/main/java/com/example/area/data/Datasource.kt
+++ mobile/app/src/main/java/com/example/area/data/Datasource.kt
@@ -1,7 +1,6 @@
 package com.example.area.data
 
 import com.example.area.R
-import com.example.area.model.Affirmation
 import com.example.area.model.AreaInfo
 
 class Datasource {
@@ -11,20 +10,6 @@ class Datasource {
         // Concat with the existing list
         return listOf<AreaInfo>(
             AreaInfo(R.drawable._5231, 0, R.drawable._968823, 0)
-        )
-    }
-    fun loadAffirmations(): List<Affirmation> {
-        return listOf<Affirmation>(
-            Affirmation(R.string.affirmation1),
-            Affirmation(R.string.affirmation2),
-            Affirmation(R.string.affirmation3),
-            Affirmation(R.string.affirmation4),
-            Affirmation(R.string.affirmation5),
-            Affirmation(R.string.affirmation6),
-            Affirmation(R.string.affirmation7),
-            Affirmation(R.string.affirmation8),
-            Affirmation(R.string.affirmation9),
-            Affirmation(R.string.affirmation10)
         )
     }
 }
--- mobile/app/src/main/java/com/example/area/model/Affirmation.kt
+++
@@ -1,4 +0,0 @@
-package com.example.area.model
-
-data class Affirmation(val stringResourceId: Int) {
-}

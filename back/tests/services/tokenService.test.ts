6fb602ecafc98478380fadac1af76d21187c0b39
ludovic-str <ludovic.sutter@epitech.eu>
2022-10-18 17:05:45 +0200

    Refacto: make token table oauth complient

--- back/tests/services/tokenService.test.ts
+++ back/tests/services/tokenService.test.ts
@@ -1,10 +1,9 @@
 import { describe, test, expect } from "@jest/globals";
-import httpStatus from "http-status";
 import { UserService, TokenService } from "../../src/services";
 
-describe("Test put token service", () => {
+describe("Test set Spotify token", () => {
   describe("Test working cases", () => {
-    test("Update one token field", async () => {
+    test("Set one token field", async () => {
       const user = await UserService.createUser(
         "Ludo",
         "Str",
@@ -12,37 +11,157 @@ describe("Test put token service", () => {
         "passwd",
       );
       const users = await UserService.getAllUsers();
-      const token = await TokenService.updateToken(
-        "Discord",
-        undefined,
-        undefined,
-        "Youtube",
-        undefined,
-        undefined,
-        users[0].id,
-      );
+      const token = await TokenService.setSpotifyToken(users[0].id, "token");
 
-      expect(token.discordToken).toBe("Discord");
-      expect(token.githubToken).toBe(null);
+      expect(token).not.toBe(null);
+      expect(token?.spotifyToken).toBe("token");
 
       await UserService.removeUserById(users[0].id);
     });
   });
   describe("Test invalid userId", () => {
     test("Put token with invalid userId", async () => {
-      try {
-        await TokenService.updateToken(
-          "Discord",
-          undefined,
-          undefined,
-          "Youtube",
-          undefined,
-          undefined,
-          1,
-        );
-      } catch (e) {
-        expect(e.status).toBe(httpStatus.BAD_REQUEST);
-      }
+      const token = await TokenService.setSpotifyToken(56, "token");
+
+      expect(token).toBe(null);
+    });
+  });
+});
+
+describe("Test set Spotify token", () => {
+  describe("Test working cases", () => {
+    test("Get one token field", async () => {
+      const user = await UserService.createUser(
+        "Ludo",
+        "Str",
+        "ludostr@mail.com",
+        "passwd",
+      );
+      const users = await UserService.getAllUsers();
+      await TokenService.setSpotifyToken(users[0].id, "token");
+      const token = await TokenService.getSpotifyToken(users[0].id);
+
+      expect(token).not.toBe(null);
+      expect(token).toBe("token");
+
+      await UserService.removeUserById(users[0].id);
+    });
+  });
+  describe("Test invalid userId", () => {
+    test("Get token with invalid userId", async () => {
+      const token = await TokenService.getSpotifyToken(56);
+
+      expect(token).toBe(null);
+    });
+  });
+});
+
+describe("Test set Github token", () => {
+  describe("Test working cases", () => {
+    test("Set one token field", async () => {
+      const user = await UserService.createUser(
+        "Ludo",
+        "Str",
+        "ludostr@mail.com",
+        "passwd",
+      );
+      const users = await UserService.getAllUsers();
+      await TokenService.setGithubToken(users[0].id, "token");
+      const token = await TokenService.getGithubToken(users[0].id);
+
+      expect(token).not.toBe(null);
+      expect(token).toBe("token");
+
+      await UserService.removeUserById(users[0].id);
+    });
+  });
+  describe("Test invalid userId", () => {
+    test("Get token with invalid userId", async () => {
+      const token = await TokenService.getGithubToken(56);
+
+      expect(token).toBe(null);
+    });
+  });
+});
+
+describe("Test get Github token", () => {
+  describe("Test working cases", () => {
+    test("Get one token field", async () => {
+      const user = await UserService.createUser(
+        "Ludo",
+        "Str",
+        "ludostr@mail.com",
+        "passwd",
+      );
+      const users = await UserService.getAllUsers();
+      const token = await TokenService.setGithubToken(users[0].id, "token");
+
+      expect(token).not.toBe(null);
+      expect(token?.githubToken).toBe("token");
+
+      await UserService.removeUserById(users[0].id);
+    });
+  });
+  describe("Test invalid userId", () => {
+    test("Put token with invalid userId", async () => {
+      const token = await TokenService.setGithubToken(56, "token");
+
+      expect(token).toBe(null);
+    });
+  });
+});
+
+describe("Test set Google token", () => {
+  describe("Test working cases", () => {
+    test("Set one token field", async () => {
+      const user = await UserService.createUser(
+        "Ludo",
+        "Str",
+        "ludostr@mail.com",
+        "passwd",
+      );
+      const users = await UserService.getAllUsers();
+      const token = await TokenService.setGoogleToken(users[0].id, "token");
+
+      expect(token).not.toBe(null);
+      expect(token?.googleToken).toBe("token");
+
+      await UserService.removeUserById(users[0].id);
+    });
+  });
+  describe("Test invalid userId", () => {
+    test("Put token with invalid userId", async () => {
+      const token = await TokenService.setGoogleToken(56, "token");
+
+      expect(token).toBe(null);
+    });
+  });
+});
+
+describe("Test set Google token", () => {
+  describe("Test working cases", () => {
+    test("Get one token field", async () => {
+      const user = await UserService.createUser(
+        "Ludo",
+        "Str",
+        "ludostr@mail.com",
+        "passwd",
+      );
+      const users = await UserService.getAllUsers();
+      await TokenService.setGoogleToken(users[0].id, "token");
+      const token = await TokenService.getGoogleToken(users[0].id);
+
+      expect(token).not.toBe(null);
+      expect(token).toBe("token");
+
+      await UserService.removeUserById(users[0].id);
+    });
+  });
+  describe("Test invalid userId", () => {
+    test("Get token with invalid userId", async () => {
+      const token = await TokenService.getGoogleToken(56);
+
+      expect(token).toBe(null);
     });
   });
 });

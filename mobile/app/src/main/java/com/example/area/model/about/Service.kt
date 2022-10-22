a2865e3817dfc36a73cf80f87593245e1434baeb
Mathieu Brujan <ken@localhost.localdomain>
2022-10-23 00:12:17 +0200

    refacto: updated about.json

--- mobile/app/src/main/java/com/example/area/model/about/Service.kt
+++ mobile/app/src/main/java/com/example/area/model/about/Service.kt
@@ -1,8 +1,10 @@
 package com.example.area.model.about
 
 data class Service (
-    val name: String,
     val id: Int,
+    val name: String,
+    val backgroundColor: String,
+    val imageUrl: String,
     val actions: List<Action>,
     val reactions: List<Reaction>
 )

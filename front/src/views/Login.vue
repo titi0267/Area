b9ecd0f5156916540a58b08b7c5e96a8a0e11b62
MaximeHff <maxime.hoffbeck@epitech.eu>
2022-10-24 23:04:55 +0200

    fix: register requirement and start css

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -2,10 +2,10 @@
     <div id="login">
         <h3>Login</h3>
         <b-field label="E-mail" label-position="on-border">
-            <b-input v-model="email"></b-input>
+            <b-input type="email" @input="checkEmail($event)" v-model="email"></b-input>
         </b-field>
         <b-field label="Password" label-position="on-border">
-            <b-input v-model="password"></b-input>
+            <b-input type="password" password-reveal v-model="password"></b-input>
         </b-field>
         <b-button @click="sendLogin()" type="is-primary">Login</b-button>
         <br/>
@@ -29,6 +29,20 @@ export default vue.extend({
 
     },
     methods: {
+        checkEmail(input) {
+            const email_regex = (/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+            // if (input == "") {
+            //     this.register.email.error = "Cannot be empty";
+            //     this.register.email.valide = false;
+            // } else if (!input.match(email_regex)) {
+            //     this.register.email.error = "One or some elements are missing in your email";
+            //     this.register.email.valide = false;
+            // } else {
+            //     this.register.email.error = "";
+            //     this.register.email.valide = true;
+            // }
+            // this.valideForm();
+        },
         async sendLogin() {
             try {
                 let {data: resp} = await this.$axios.post('/users/login', {

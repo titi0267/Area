f8b0a6a181d40a7411fd1ba6c1e5e539a28ea856
Mathieu Brujan <ken@localhost.localdomain>
2022-10-30 15:55:19 +0100

    feat: add a new fragment that can edit ip and port set at the beginning of the program

--- mobile/app/src/main/java/com/example/area/fragment/area/ProfileFragment.kt
+++ mobile/app/src/main/java/com/example/area/fragment/area/ProfileFragment.kt
@@ -1,6 +1,7 @@
 package com.example.area.fragment.area
 
 import android.content.Intent
+import android.hardware.Camera.Area
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import com.example.area.R
 import com.example.area.activity.AreaActivity
 import com.example.area.activity.MainActivity
 import com.example.area.utils.SessionManager
+import com.google.android.material.textview.MaterialTextView
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
     override fun onCreateView(
@@ -20,6 +22,9 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
     ): View? {
         val view = super.onCreateView(inflater, container, savedInstanceState) ?: return null
         val sessionManager = SessionManager(context as AreaActivity)
+        val ipPortUrl = (sessionManager.fetchAuthToken("url") ?: return null).dropLast(1).drop(7)
+
+        view.findViewById<MaterialTextView>(R.id.profile_current_ip_text_value).text = ipPortUrl
         view.findViewById<Button>(R.id.backButton).setOnClickListener {
             (context as AreaActivity).onBackPressed()
         }
@@ -32,6 +37,9 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
                     MainActivity::class.java
                 )
             )
+        }
+        view.findViewById<Button>(R.id.profile_change_ip_port_button).setOnClickListener {
+            (context as AreaActivity).changeFragment(ChangeIpPortFragment(), "change_ip_port")
         }
         return view
     }

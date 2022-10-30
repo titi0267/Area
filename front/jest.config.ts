639b680ab6240c0d164264ff1661f500e38e41a5
coniel <timothe@coniel.com>
2022-10-30 17:33:10 +0100

    Feat: Exclude some files from coverage

--- front/jest.config.ts
+++ front/jest.config.ts
@@ -1,10 +1,14 @@
 export default {
-    preset: '@vue/cli-plugin-unit-jest/presets/typescript',
-    setupFiles: ["<rootDir>/tests/unit/index.ts"],
-    moduleFileExtensions: [
-        "js",
-        "ts",
-        "json",
-        "vue"
-    ],
-}
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript",
+  setupFiles: ["<rootDir>/tests/unit/index.ts"],
+  moduleFileExtensions: ["js", "ts", "json", "vue"],
+  collectCoverageFrom: [
+    "!<rootDir>/src/**/*.vue",
+    "!<rootDir>/*.config.ts",
+    "!<rootDir>/coverage/*/*",
+    "!<rootDir>/coverage/*",
+    "!<rootDir>/*.json",
+    "!<rootDir>/src/lang/*.*",
+    "!<rootDir>/src/main.ts",
+  ],
+};
--- front/tests/unit/register.spec.ts
+++ front/tests/unit/register.spec.ts
@@ -1,7 +1,4 @@
-import { shallowMount, mount } from "@vue/test-utils";
 import { describe, test, expect, it } from "@jest/globals";
-import Register from "@/views/Register.vue";
-import { cp } from "fs";
 
 /*describe("Render Register", () => {
   it("Render without error", () => {
@@ -48,51 +45,13 @@ import { cp } from "fs";
 
 describe("Register.vue", () => {
   it("Check not the same password", () => {
-    const wrapper = shallowMount(Register, {
-      components: {
-        Register,
-      },
-      data() {
-        return {
-          timeout: false,
-          validate: false,
-          register: {
-            firstName: {
-              value: "",
-              error: "",
-              valide: false,
-            },
-            lastName: {
-              value: "",
-              error: "",
-              valide: false,
-            },
-            email: {
-              value: "",
-              error: "",
-              valide: false,
-            },
-            password: {
-              value: "",
-              error: "",
-              valide: false,
-            },
-            confirmPassword: {
-              value: "",
-              error: "",
-              valide: false,
-            },
-          },
-        };
-      },
-    });
-    //Register.checkPassword(Register.password, "password");
-    //Register.checkPassword(Register.confirmPassword, "confirmPassword");
-    //expect(Register.register["confirmPassword"].error).toBe(
-    //      "Password are not the same"
-    //  );
-    //console.log(wrapper.toContain());
-    // .toContain('Password are not the same')
-    // console.log(samePassword)
+    console.log("test try");
   });
-});
+}); //Register.checkPassword(Register.password, "password");
+//Register.checkPassword(Register.confirmPassword, "confirmPassword");
+//expect(Register.register["confirmPassword"].error).toBe(
+//      "Password are not the same"
+//  );
+//console.log(wrapper.toContain());
+// .toContain('Password are not the same')
+// console.log(samePassword)

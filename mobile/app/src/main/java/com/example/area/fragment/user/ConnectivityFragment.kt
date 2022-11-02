bd524259c57f36f5eae9be8b4d140ffdf8890ad5
Mathieu Brujan <ken@localhost.localdomain>
2022-11-02 19:44:27 +0100

    feat: add new fragment that responsible for the selection of ip/port to separate login register and connectivity

---
+++ mobile/app/src/main/java/com/example/area/fragment/user/ConnectivityFragment.kt
@@ -0,0 +1,74 @@
+package com.example.area.fragment.user
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.fragment.app.Fragment
+import com.example.area.R
+import com.example.area.activity.UserConnectionActivity
+import com.example.area.utils.*
+
+class ConnectivityFragment : Fragment(R.layout.fragment_connectivity) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState) ?: return null
+
+        connectivityFocusListener(view)
+        view.findViewById<Button>(R.id.connectivity_test_connection_button).setOnClickListener {
+            testConnection(view)
+        }
+        view.findViewById<Button>(R.id.connectivity_apply_url_button).setOnClickListener {
+            applyURL(view)
+        }
+        return (view)
+    }
+
+    private fun testConnection(view: View) {
+        val url = getUrlFromFields(view) ?: return
+    }
+
+    private fun applyURL(view: View) {
+        val url = getUrlFromFields(view) ?: return
+        val sessionManager = SessionManager(context as UserConnectionActivity)
+
+        //TODO(if connection valid)
+        sessionManager.saveAuthToken("url", url)
+        (context as UserConnectionActivity).changeFragment(NotLoggedInFragment(),"not_logged_in")
+    }
+
+    private fun getUrlFromFields(view: View): String? {
+        return try {
+            (urlParser(
+                view.findViewById<EditText>(R.id.connectivity_ip_field_edit_text).text.toString(),
+                view.findViewById<EditText>(R.id.connectivity_port_field_edit_text).text.toString()
+            ))
+        }
+        catch (e: IllegalArgumentException) {
+            Toast.makeText(context as UserConnectionActivity, e.message, LENGTH_SHORT).show()
+            (null)
+        }
+    }
+
+    private fun connectivityFocusListener(view: View) {
+        textFieldsFocusListener(
+            view,
+            R.id.connectivity_ip_field_edit_text,
+            R.id.connectivity_ip_field_layout,
+            ::checkIp
+        )
+        textFieldsFocusListener(
+            view,
+            R.id.connectivity_port_field_edit_text,
+            R.id.connectivity_port_field_layout,
+            ::checkPort
+        )
+    }
+}

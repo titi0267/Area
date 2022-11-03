e66afc75a8104c469afff4a31bbc1d60136e68da
coniel <timothe@coniel.com>
2022-11-03 16:53:22 +0100

    Fix: request changes

--- back/src/area/spotify/spotify.reactions.ts
+++ back/src/area/spotify/spotify.reactions.ts
@@ -10,40 +10,30 @@ const addTrackToPlaylist = async (
   const spotifyApi = await ServiceHelper.getSpotifyClient(userId);
 
   if (!spotifyApi) return;
-  const playlistId = ServiceHelper.getSpotifyPlaylistId(reactionParam);
 
-  if (playlistId == null) return;
+  if (reactionParam == undefined || null) return;
 
   const myPlaylists = await spotifyApi.getUserPlaylists();
   let areaPlaylistId = null;
-  myPlaylists.body.items.forEach(element => {
-    if (element.name == "Area") {
-      areaPlaylistId = element.id;
-    }
+
+  myPlaylists.body.items.find(elem => {
+    if (elem.name === "Area") areaPlaylistId = elem.id;
   });
+
   if (areaPlaylistId == null) {
-    const newPlaylistId = await spotifyApi.createPlaylist("Area", {
+    areaPlaylistId = await spotifyApi.createPlaylist("Area", {
       description: "This is the area playlist",
       collaborative: false,
       public: true,
     });
-
-    const trackToAdd = await spotifyApi.searchTracks("Tu veux mon zizi");
-
-    if (trackToAdd.body.tracks?.items[0].id == undefined) return;
-
-    await spotifyApi.addTracksToPlaylist(newPlaylistId.body.id, [
-      trackToAdd.body.tracks?.items[0].uri,
-    ]);
-  } else {
-    const trackToAdd = await spotifyApi.searchTracks("Tu veux mon zizi");
-
-    if (trackToAdd.body.tracks?.items[0].uri == undefined) return;
-
-    await spotifyApi.addTracksToPlaylist(areaPlaylistId, [
-      trackToAdd.body.tracks?.items[0].uri,
-    ]);
   }
+  const trackToAdd = await spotifyApi.searchTracks(reactionParam);
+
+  if (trackToAdd.body.tracks?.items[0].id == undefined) return;
+
+  await spotifyApi.addTracksToPlaylist(areaPlaylistId.body.id, [
+    trackToAdd.body.tracks?.items[0].uri,
+  ]);
 };
 
 const updateVolume = async (
@@ -53,7 +43,9 @@ const updateVolume = async (
   const spotifyApi = await ServiceHelper.getSpotifyClient(userId);
 
   if (!spotifyApi) return;
+
   const device = await spotifyApi.getMyDevices();
+
   device.body.devices.forEach(element => {
     if (element.is_active == true) {
       if (element.type != "Smartphone" && element.id != null) {
@@ -70,8 +62,9 @@ const startMusic = async (
   const spotifyApi = await ServiceHelper.getSpotifyClient(userId);
 
   if (!spotifyApi) return;
+  if (reactionParam == undefined || null) return;
 
-  const trackToPlay = await spotifyApi.searchAlbums("Tu veux mon zizi");
+  const trackToPlay = await spotifyApi.searchAlbums(reactionParam);
 
   if (trackToPlay.body.albums == undefined) return;
   const start = await spotifyApi.play({
@@ -86,8 +79,9 @@ const addMusicToQueue = async (
   const spotifyApi = await ServiceHelper.getSpotifyClient(userId);
 
   if (!spotifyApi) return;
+  if (reactionParam == undefined || null) return;
 
-  const trackToAdd = await spotifyApi.searchTracks("Tu veux mon zizi");
+  const trackToAdd = await spotifyApi.searchTracks(reactionParam);
 
   if (trackToAdd.body.tracks?.items[0].uri == undefined) return;
 

897c623904f3108a94b7051c9e19df41bbde15b8
ludovic-str <ludovic.sutter@epitech.eu>
2022-11-04 16:06:02 +0100

    Feat: add weather action weather clear

--- back/src/index.ts
+++ back/src/index.ts
@@ -7,6 +7,7 @@ import fastifyCors from "@fastify/cors";
 import ENV from "./env";
 import { UserInfos } from "./types/global.types";
 import areaLoop from "./area/loop.area";
+import { weatherBecameClear } from "./area/weather/weather.action";
 
 const prisma = new PrismaClient();
 const server = fastify();
@@ -40,8 +41,21 @@ const main = async () => {
 };
 
 setInterval(async () => {
-  await areaLoop();
-}, 0.5 * 60 * 1000);
+  //await areaLoop();
+  await weatherBecameClear({
+    id: 1,
+    actionId: 1,
+    actionServiceId: 1,
+    actionParam: "Strasbourg",
+    reactionId: 1,
+    reactionParam: "",
+    reactionServiceId: 1,
+    userId: 799,
+    lastActionFetch: new Date(),
+    lastActionValue: null,
+    enabled: true,
+  });
+}, 0.1 * 60 * 1000);
 
 main()
   .catch(e => {

0ad950aed9f5c3e920b234c646e675273306c770
coniel <timothe@coniel.com>
2022-11-04 12:50:52 +0100

    Feat: No oauth if token is already in database

--- front/src/components/SelectService.vue
+++ front/src/components/SelectService.vue
@@ -2,27 +2,47 @@
   <div id="SelectService">
     <h2>Select your {{ type }} service name</h2>
     <div>
-      <b-input class="search-input" @input="debounceInput" placeholder="Enter your service here"></b-input>
+      <b-input
+        class="search-input"
+        @input="debounceInput"
+        placeholder="Enter your service here"
+      ></b-input>
       <div class="services">
         <div v-for="service in services" :key="service.name">
-          <div class="service"
+          <div
+            class="service"
             :style="{ 'background-color': service.backgroundColor }"
             :class="{ selected: service.id == area[type + 'ServiceId'] }"
-            v-if="service[type + 's'].length != 0 && service.name.toLowerCase().includes(filterInput.toLowerCase())"
-            @click="$emit(type + 'ServiceId', service.id), $emit('save'), getOAuthUrl()">
-                <b-image :src="service.imageUrl"></b-image>
-                <p> {{ service.name }} </p>
+            v-if="
+              service[type + 's'].length != 0 &&
+              service.name.toLowerCase().includes(filterInput.toLowerCase())
+            "
+            @click="
+              $emit(type + 'ServiceId', service.id),
+                $emit('save'),
+                getOAuthUrl(),
+                getTokenTable()
+            "
+          >
+            <b-image :src="service.imageUrl"></b-image>
+            <p>{{ service.name }}</p>
           </div>
         </div>
       </div>
     </div>
     <div class="buttons">
-        <b-button @click="$emit('previous'), $emit('save'), $router.push(area.state == -1 ? '/home' : '/create/action')">
-          Previous
-        </b-button>
-        <b-button @click="$emit('next'), $emit('save'), $emit('loading'), redirectOAuth()" :disabled="oauthURL == ''">
-          Next
-        </b-button>
+      <b-button
+        @click="
+          $emit('previous'),
+            $emit('save'),
+            $router.push(area.state == -1 ? '/home' : '/create/action')
+        "
+      >
+        Previous
+      </b-button>
+      <b-button @click="$emit('next'), $emit('save'), redirectOAuth()">
+        Next
+      </b-button>
     </div>
   </div>
 </template>
@@ -35,21 +55,27 @@ export default vue.extend({
   data() {
     return {
       filterInput: "" /** It's a filter input used for search a service */,
-      oauthURL: "" /** This variable contains the oAuth URL of the right service */,
+      oauthURL:
+        "" /** This variable contains the oAuth URL of the right service */,
+      generateOauthURL:
+        false /** Set to true if no token are assigned to the selected service */,
     };
   },
   mounted() {
-    this.$nextTick(() => this.getOAuthUrl());
+    this.$nextTick(() => this.getTokenTable());
+    if (this.generateOauthURL == true) this.$nextTick(() => this.getOAuthUrl());
   },
   watch: {
-    services: function(): void {
+    services: function (): void {
+      this.$nextTick(() => this.getTokenTable());
+      if (this.generateOauthURL == true)
         this.$nextTick(() => this.getOAuthUrl());
     },
   },
   props: {
-    type: String, /** Type between 'action' or 'reaction' */
-    services: Array, /** Array that contains the About.JSON file */
-    area: Object, /** Object that contains the area creation fields */
+    type: String /** Type between 'action' or 'reaction' */,
+    services: Array /** Array that contains the About.JSON file */,
+    area: Object /** Object that contains the area creation fields */,
   },
   methods: {
     /**
@@ -67,18 +93,43 @@ export default vue.extend({
      * @data {String} type
      * @async
      */
-    async getOAuthUrl(): Promise<any> {
+    async getTokenTable(): Promise<void> {
       try {
-        let serviceName = this.services.find(service => service.id == this.area[this.type + "ServiceId"]).oauthName;
-        if (serviceName == null) return;
-        const { data: url } = await this.$axios.get("/oauth/" + serviceName + "/link/front", {
-            headers: {
-                Authorization: this.$store.getters.userToken || "noToken",
-            }
+        const { data: user } = await this.$axios.get("/users/me", {
+          headers: {
+            Authorization: this.$store.getters.userToken || "noToken",
+          },
         });
+        let serviceName = this.services.find(
+          (service) => service.id == this.area[this.type + "ServiceId"]
+        ).oauthName;
+        if (serviceName == null) return;
+        if (user.tokensTable[serviceName.toLowerCase() + "Token"] != null) {
+          this.oauthURL = "";
+          this.generateOauthURL = false;
+          return;
+        }
+        this.generateOauthURL = true;
+      } catch {}
+    },
+    async getOAuthUrl(): Promise<void> {
+      try {
+        let serviceName = this.services.find(
+          (service) => service.id == this.area[this.type + "ServiceId"]
+        ).oauthName;
+
+        if (serviceName == null) return;
+        const { data: url } = await this.$axios.get(
+          "/oauth/" + serviceName + "/link/front",
+          {
+            headers: {
+              Authorization: this.$store.getters.userToken || "noToken",
+            },
+          }
+        );
         this.oauthURL = url;
       } catch {
-        this.oauthURL = '';
+        this.oauthURL = "";
       }
     },
     /**
@@ -87,70 +138,71 @@ export default vue.extend({
      */
     redirectOAuth(): void {
       window.location.href = this.oauthURL;
-    }
+    },
   },
 });
 </script>
 
 <style scoped lang="scss">
 #SelectService {
-    padding: 20px;
-    padding-top: 0px;
+  padding: 20px;
+  padding-top: 0px;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .search-input {
+    display: flex;
+    justify-content: center;
+    :deep(input) {
+      width: 400px;
+      margin: 10px;
+    }
+  }
+  h2 {
+    font-family: "Courier New", Courier, monospace;
+  }
+  .buttons {
+    display: flex;
+    position: absolute;
+    padding: 0px 30px;
+    left: 0;
     width: 100%;
+    bottom: 20px;
+    justify-content: space-between;
+    :deep(button) {
+      width: 100px;
+      span,
+      a {
+        color: hsl(0deg, 0%, 21%);
+      }
+    }
+  }
+  .services {
+    margin: 10px;
+    margin: 10px;
+    display: flex;
     height: 100%;
     position: relative;
-    .search-input {
-        display: flex;
-        justify-content: center;
-        :deep(input) {
-            width: 400px;
-            margin: 10px;
-        }
+    justify-content: center;
+    .service {
+      height: 150px;
+      width: 150px;
+      border-radius: 10px;
+      margin: 5px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      cursor: pointer;
+      padding: 5px;
+      p {
+        font-size: 20px;
+        font-family: Hitmo Regular;
+        text-transform: uppercase;
+      }
+      &.selected:not(p) {
+        outline: 2px solid black;
+      }
     }
-    h2 {
-        font-family: 'Courier New', Courier, monospace;
-    }
-    .buttons {
-        display: flex;
-        position: absolute;
-        padding: 0px 30px;
-        left: 0;
-        width: 100%;
-        bottom: 20px;
-        justify-content: space-between;
-        :deep(button) {
-            width: 100px;
-            span, a {
-                color: hsl(0deg, 0%, 21%);
-            }
-        }
-    }
-    .services {
-        margin: 10px;
-        margin: 10px;
-        display: flex;
-        height: 100%;
-        position: relative;
-        justify-content: center;
-        .service {
-            height: 150px;
-            width: 150px;
-            border-radius: 10px;
-            margin: 5px;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            cursor: pointer;
-            padding: 5px;
-            p {
-                font-size: 20px;
-                font-family: Hitmo Regular;
-                text-transform: uppercase;
-            }
-            &.selected:not(p) {
-                outline: 2px solid black;
-            }
-        }
-    }
+  }
 }
 </style>

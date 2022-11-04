fedb19c7e1f95a8108a15c94a7a0bf26041678d5
Frantz <alexandre.frantz@epitech.eu>
2022-11-04 20:57:17 +0100

    refacto: better enable and delete

--- mobile/app/src/main/java/com/example/area/fragment/area/AreaListItemFragment.kt
+++ mobile/app/src/main/java/com/example/area/fragment/area/AreaListItemFragment.kt
@@ -1,7 +1,6 @@
 package com.example.area.fragment.area
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,11 +20,14 @@ import com.example.area.utils.AboutJsonCreator
 import com.example.area.utils.SessionManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.area.repository.Repository
 
 class AreaListItemFragment(private val item: ActionReaction) : Fragment(R.layout.fragment_area_list_item) {
 
     private var abt: About? = null
     private lateinit var viewModel: MainViewModel
+    private lateinit var enable: EnableDisable
+    private var enabledStatus = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,12 +37,10 @@ class AreaListItemFragment(private val item: ActionReaction) : Fragment(R.layout
         val view = super.onCreateView(inflater, container, savedInstanceState) ?: return null
         val about = AboutJsonCreator()
         val sessionManager = SessionManager(context as AreaActivity)
-        val token = sessionManager.fetchAuthToken("user_token") ?: null
-        var enable: EnableDisable
+        val token = sessionManager.fetchAuthToken("user_token") ?: return view
         val url = sessionManager.fetchAuthToken("url") ?: return view
         val rep = Repository(url)
         val viewModelFactory = MainViewModelFactory(rep)
-        var enabledStatus = 0
 
         viewModel = ViewModelProvider(context as AreaActivity, viewModelFactory)[MainViewModel::class.java]
         view.findViewById<Button>(R.id.backFromAreaListItemButton).setOnClickListener {
@@ -58,33 +58,37 @@ class AreaListItemFragment(private val item: ActionReaction) : Fragment(R.layout
             }
         }
         view.findViewById<Switch>(R.id.enableItemListSwitch).setOnCheckedChangeListener { _, isChecked ->
-            enable = EnableDisable(item.id, isChecked)
-            if (token != null) {
-                enabledStatus=0
-                viewModel.putEnableDisable(token, enable)
-                viewModel.enableResponse.observe(context as AreaActivity, Observer { response ->
-                    if (response.isSuccessful) {
-                        if (enabledStatus == 0) {
-                            Toast.makeText(context as AreaActivity, (if (isChecked) "Enabled" else "Disabled"), Toast.LENGTH_SHORT).show()
-                            enabledStatus++
-                        }
-                    }
-                })
-            }
+            onEnableDisableSwitch(isChecked, token)
         }
         view.findViewById<Button>(R.id.deleteItemListButton).setOnClickListener {
-            if (token != null) {
-                viewModel.deleteArea(token, item.id)
-                viewModel.deleteAreaResponse.observe(context as AreaActivity, Observer { response ->
-                    if (response.isSuccessful) {
-                        Toast.makeText(context as AreaActivity, "Area successfully deleted", Toast.LENGTH_SHORT).show()
-                        (context as AreaActivity).changeFragment(AreaListFragment(), "area_list")
-                    }
-                    Log.d("Delete response", response.toString())
-                })
-
-            }
+            onDeleteButton(token)
         }
         return view
+    }
+
+    private fun onEnableDisableSwitch(isChecked: Boolean, token: String?) {
+        enable = EnableDisable(item.id, isChecked)
+        if (token == null)
+            return
+        enabledStatus=0
+        viewModel.putEnableDisable(token, enable)
+        viewModel.enableResponse.observe(context as AreaActivity, Observer { response ->
+            if (response.isSuccessful && enabledStatus == 0) {
+                Toast.makeText(context as AreaActivity, (if (isChecked) "Enabled" else "Disabled"), Toast.LENGTH_SHORT).show()
+                enabledStatus++
+            }
+        })
+    }
+
+    private fun onDeleteButton(token: String?) {
+        if (token != null) {
+            viewModel.deleteArea(token, item.id)
+            viewModel.deleteAreaResponse.observe(context as AreaActivity, Observer { response ->
+                if (response.isSuccessful) {
+                    Toast.makeText(context as AreaActivity, "Area successfully deleted", Toast.LENGTH_SHORT).show()
+                    (context as AreaActivity).changeFragment(AreaListFragment(), "area_list")
+                }
+            })
+        }
     }
 }

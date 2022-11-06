9822581a1e934d1fee532076943aef2578e89718
Mathieu Brujan <ken@localhost.localdomain>
2022-11-06 14:24:26 +0100

    refacto: safer utilization of delete area route in AreaListItemFragment.kt

--- mobile/app/src/main/java/com/example/area/fragment/area/AreaListItemFragment.kt
+++ mobile/app/src/main/java/com/example/area/fragment/area/AreaListItemFragment.kt
@@ -21,6 +21,7 @@ import com.example.area.utils.SessionManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.area.repository.Repository
+import retrofit2.Response
 
 class AreaListItemFragment(private val item: ActionReaction) : Fragment(R.layout.fragment_area_list_item) {
 
@@ -76,14 +77,19 @@ class AreaListItemFragment(private val item: ActionReaction) : Fragment(R.layout
     }
 
     private fun onDeleteButton(token: String?) {
+        val observer: Observer<Response<ActionReaction>?> = Observer { response ->
+            if (response == null) {
+                return@Observer
+            }
+            if (response.isSuccessful) {
+                Toast.makeText(context as AreaActivity, "Area successfully deleted", Toast.LENGTH_SHORT).show()
+                (context as AreaActivity).changeFragment(AreaListFragment(), "area_list")
+            }
+        }
+
         if (token != null) {
-            viewModel.deleteArea(token, item.id)
-            viewModel.deleteAreaResponse.observe(context as AreaActivity, Observer { response ->
-                if (response.isSuccessful) {
-                    Toast.makeText(context as AreaActivity, "Area successfully deleted", Toast.LENGTH_SHORT).show()
-                    (context as AreaActivity).changeFragment(AreaListFragment(), "area_list")
-                }
-            })
+            viewModel.deleteArea(token, item.id, context as AreaActivity, observer)
+            viewModel.deleteAreaResponse.observe(context as AreaActivity, observer)
         }
     }
 }

11f10aff331de06c8ab2da986d38b035add24468
MaximeHff <maxime.hoffbeck@epitech.eu>
2022-11-07 22:41:36 +0100

    feat: edit css

--- front/src/components/EditArea.vue
+++ front/src/components/EditArea.vue
@@ -1,18 +1,24 @@
 <template>
-    <div id="EditArea">
-        <p>Action param</p>
-        <b-input></b-input>
-        <b-button @click="deleteArea()">Delete</b-button>
-        <b-button @click="enableDisableArea()">Enable-disable</b-button>
-        <p>Reaction param</p>
-        <b-input></b-input>
+    <div id="EditArea" :style="{ 'background' : `linear-gradient(to right, ${action ? action.backgroundColor : ''}, ${reaction ? reaction.backgroundColor : ''})` }">
+        <div class="action-param">
+            <p>Action parameter</p>
+            <b-input v-model="area.actionParam"></b-input>
+        </div>
+        <div class="manage">
+            <div class="edit">
+                <b-icon icon="trash" @click.native="deleteArea()"></b-icon>
+            </div>
+            <b-switch size="is-medium" passive-type="is-danger" type="is-success" v-model="area.enabled" @input="enableDisableArea()"></b-switch>
+        </div>
+        <div class="reaction-param">
+            <p>Reaction parameter</p>
+            <b-input v-model="area.actionParam"></b-input>
+        </div>
     </div>
 </template>
 
 <script scoped lang="ts">
 import vue from 'vue';
-import _ from "lodash";
-
 
 export default vue.extend({
     data() {
@@ -20,11 +26,13 @@ export default vue.extend({
         }
     },
     props: {
-        id: Number,
+        area: Object,
+        action: Object,
+        reaction: Object,
     },
     methods: {
         async deleteArea(): Promise<void> {
-            await this.$axios.delete("/areas/" + this.id, {
+            await this.$axios.delete("/areas/" + this.area.id, {
                 headers: {
                     Authorization: this.$store.getters.userToken || "noToken",
                 }
@@ -32,22 +40,82 @@ export default vue.extend({
             this.$emit('deleted')
         },
         async enableDisableArea(): Promise<void> {
-            const {data : area} = await this.$axios.get("/areas/" + this.id, {
-                headers: {
-                    Authorization: this.$store.getters.userToken || "noToken",
-                }
-            })
-            const newArea = {
-                areaId: area.id,
-                enabled: !area.enabled,
+            try {
+                await this.$axios.put("/areas/", {
+                    areaId: this.area.id,
+                    enabled: this.area.enabled,
+                    actionParam: this.area.actionParam,
+                    reactionParam: this.area.reactionParam
+                }, {
+                    headers: {
+                        Authorization: this.$store.getters.userToken || "noToken",
+                    }
+                })
+            } catch {
+                
             }
-            console.log(newArea)
-            await this.$axios.put("/areas/", newArea, {
-                headers: {
-                    Authorization: this.$store.getters.userToken || "noToken",
-                }
-            })
         }
     }
 });
 </script>
+
+<style scoped lang="scss">
+#EditArea {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: auto;
+    margin-bottom: 30px;
+    width: 90%;
+    background-color: red;
+    height: 110px;
+    padding: 10px 30px;
+    border-radius: 20px;
+    .action-param,
+    .reaction-param {
+        color: white;
+        font-family: "Avenir Roman";
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: center;
+        text-align: start;
+        width: 100%;
+        :deep(.control) {
+        width: 100%;
+            input {
+                width: 80%;
+            }
+        }
+    }
+    .reaction-param {
+        align-items: flex-end;
+        text-align: end;
+    }
+    .manage {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        :deep(label) {
+            margin-right: 0px;
+        }
+        .edit {
+            display: flex;
+            align-items: center;
+            text-transform: uppercase;
+            :deep(span) {
+                padding: 20px;
+                box-shadow: 0 0 15px 1px #000000a1;
+                background-color: white;
+                border-radius: 50%;
+                cursor: pointer;
+                svg {
+                    width: 19px;
+                    height: 19px;
+                }
+            }
+        }
+    }
+}
+</style>

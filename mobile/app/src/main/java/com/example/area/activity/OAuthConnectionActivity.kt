b87d387595eb572d5cea1ca1b6f71716b34ff7db
Mathieu Brujan <ken@localhost.localdomain>
2022-11-10 14:53:39 +0100

    merge: merge from develop

--- mobile/app/src/main/java/com/example/area/activity/OAuthConnectionActivity.kt
+++ mobile/app/src/main/java/com/example/area/activity/OAuthConnectionActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider

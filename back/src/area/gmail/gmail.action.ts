47a9b7c648efe2a9c859851b258cfed27e863399
coniel <timothe@coniel.com>
2022-11-11 19:48:20 +0100

    Fix: New mail doesn't update the value when its the first one

--- back/src/area/gmail/gmail.action.ts
+++ back/src/area/gmail/gmail.action.ts
@@ -17,8 +17,9 @@ const newMailFrom = async (area: Area): Promise<string | null> => {
       q: "from:" + area.actionParam,
     })
   ).data;
-
-  if (!messages.resultSizeEstimate) return null;
+  console.log(messages.resultSizeEstimate)
+  if (messages.resultSizeEstimate === undefined || messages.resultSizeEstimate === null) return null;
+  console.log("last action")
   if (area.lastActionValue === null) {
     await AreaService.updateAreaValues(
       area.id,

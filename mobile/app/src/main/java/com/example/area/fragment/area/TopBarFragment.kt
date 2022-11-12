4165bb8741a36898f505c4e77f47c799f5da50c1
Mathieu Brujan <ken@localhost.localdomain>
2022-11-12 13:23:23 +0100

    feat: version without onBackPressed

--- mobile/app/src/main/java/com/example/area/fragment/area/TopBarFragment.kt
+++ mobile/app/src/main/java/com/example/area/fragment/area/TopBarFragment.kt
@@ -9,17 +9,4 @@ import androidx.fragment.app.Fragment
 import com.example.area.R
 import com.example.area.activity.AreaActivity
 
-class TopBarFragment : Fragment(R.layout.fragment_top_bar) {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = super.onCreateView(inflater, container, savedInstanceState) ?: return null
-
-        view.findViewById<ImageButton>(R.id.top_bar_back_button).setOnClickListener {
-            (context as AreaActivity).onBackPressed()
-        }
-        return view
-    }
-}
+class TopBarFragment : Fragment(R.layout.fragment_top_bar) {}
